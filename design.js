2812106fec1819c48c803c390fdf8494b3737f20
jettyboop <widodoj@student.unimelb.edu.au>
2025-06-01 02:32:34 +1000

    final change (hopefully)

--- design.js
+++ design.js
@@ -36,6 +36,35 @@ let snailActive = [];
 
 let leaderboardY = [];
 
+let isSmiling = false; 
+
+let snailInLoveImages = []; 
+let snailInLoveWithUser = -1;
+let loveReady = false;
+let loveTriggered = false;
+
+
+
+let fly = {
+  x: 0,
+  y: 0,
+  angle: 0,
+  baseAngle: 0,
+  speed: 2,
+  history: []
+};
+let flyNoiseOffset = 0;
+let flyActive = false;
+let flyImg;
+
+let staringTime = 0;
+let raceTime = 0; 
+let lastTimerUpdate = 0;
+
+let snailMessages = [];
+const maxMessages = 3; 
+
+let messageYPositions = [];
 
 
 function preload() {
@@ -43,39 +72,43 @@ snailImages[0] = loadImage("snails/gary.png");
 snailImages[1] = loadImage("snails/turbo.png");
 snailImages[2] = loadImage("snails/steve.png");
 snailImages[3] = loadImage("snails/escargot.png");
+snailInLoveImages[0] = loadImage("snails/garyinlove.png");
+snailInLoveImages[1] = loadImage("snails/turboinlove.png");
+snailInLoveImages[2] = loadImage("snails/steveinlove.png");
+snailInLoveImages[3] = loadImage("snails/escargotinlove.png");
+flyImg = loadImage("snails/fly.png");
 }
 
+
+
 let flipMessages = [
-  "did a backflip and could't get back up.",
-  "attempted a backflip to impress the crowd and failed. (you missed it, of course)",
-  "got too excited and fell on its back.",
-  "tried to do gymnastics instead of racing."
+  "did a backflip and could't get back up",
+  "attempted a backflip to impress the crowd and failed (you missed it, of course)",
+  "got too excited and fell on its back",
+  "tried to do gymnastics instead of racing"
 ];
 
 let eatenMessages = [
   "{EATER} ate {VICTIM}",
   "{EATER} was hungry and saw {VICTIM}",
-  "{VICTIM} is gone, {EATER}",
+  "{VICTIM} is gone, {EATER} was hungry",
 ];
 
 let wrongWayMessages = [
-  "panicked and turned around.",
-  "thought backwards was faster.",
-  "took a wrong turn but is committing to it with confidence.",
-  "raged quit and is going home to see its wife and kids."
+  "panicked and turned around",
+  "thought backwards was faster",
+  "took a wrong turn but is committing to it with confidence",
+  "raged quit and is going home to see its wife and kids"
 ];
 
 let randomSnailMessages = [
-  "got bored and is wandering around.",
-  "gave up racing and is exploring other pastures.",
-  "forgot it's in a race.",
-  "saw a butterfly and is trying to catch it."
+  "got bored and is wandering around",
+  "gave up racing and is exploring other pastures",
+  "forgot it's in a race",
+  "saw a butterfly and is trying to catch it"
 ];
 
-let cannibalMessages = [
-  "{EATER} got hungry and ate {VICTIM}. RIP.",
-  "{EATER} ate {VICTIM}. RIP.",
-];
+
 
 let winCaptions = [
     "won and you missed the race",
@@ -86,11 +119,11 @@ let winCaptions = [
 ];
 
 
-function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.id("canvas");
+function setup() {canvas = createCanvas(window.innerWidth, window.innerHeight);
+    canvas.position(0, 0);
+  canvas.style('display', 'block');
   finishLine = width - 60;
-  textFont('Inter Tight');
+  textFont("Helvetica");
 
    // Set a slow frame rate.
    frameRate(30);
@@ -141,6 +174,13 @@ function faceReady() {
   faceapi.detect(gotFaces);
 }
 
+let inLoveMessages = [
+    "saw someone smiling and fell in love",
+    "got shy",
+    "fell in love with you",
+    "fell in love"
+  ];
+
 function gotFaces(error, result) {
   if (error) {
     console.log(error);
@@ -148,6 +188,43 @@ function gotFaces(error, result) {
   }
 
   detections = result; //Now all the data in this detections
+  
+    isSmiling = false;
+  if (detections.length > 0 && detections[0].expressions) {
+  let expressions = detections[0].expressions;
+  
+  if (expressions.happy > 0.85) { 
+    isSmiling = true;
+    if (!loveTriggered && random() < 0.1) {
+      let validSnails = [];
+      for (let i = 0; i < numSnails; i++) {
+        if (!snailEaten[i]) validSnails.push(i);
+      }
+  
+      if (validSnails.length > 0) {
+        snailInLoveWithUser = random(validSnails);
+        loveTriggered = true;
+  
+       let newMessage = `${snailNames[snailInLoveWithUser]} ${random(inLoveMessages)}`;
+        snailMessages.push(newMessage);
+        messageYPositions.push(height + 50);
+        if (snailMessages.length > maxMessages) snailMessages.shift();
+  }
+
+     }
+   }
+
+   if (expressions.surprised > 0.8 && !flyActive) {
+    flyActive = true;
+    fly.x = random(width);
+    fly.y = random(height);
+    fly.history = [];
+    fly.angle = random(TWO_PI);
+    fly.baseAngle = fly.angle;
+  }
+ }
+  
+  
   // console.log(detections);
   //clear();//Draw transparent background
   drawBoxs(detections);//Draw detection box
@@ -184,29 +261,38 @@ function drawLandmarks(detections){
 
 //code edited and tweaked from https://editor.p5js.org/ratematica/sketches/WytTUH_wa
 
-function drawSnail(x, y, i) {
-  if (snailEaten[i]) return; // invisible if eaten
+function getSnailY(i) {
+  let spacing = height / (numSnails + 1);
+  return spacing * (i + 1);
+}
 
+function drawSnail(x, y, i) {
+  if (snailEaten[i]) return; 
+  let img = (i === snailInLoveWithUser) ? snailInLoveImages[i] : snailImages[i]; 
+  
   push();
   translate(x, y);
 
   if (snailFlip[i]) {
     scale(1, -1);
-    image(snailImages[i], -90, -150, 200, 200);
+    image(img, -90, -150, 200, 200);
     scale(1, -1);
     fill(0);
+    textSize(18);
     textAlign(LEFT, CENTER);
     text(snailNames[i], 90, 50);
   } else if (snailWrongWay[i]) {
     scale(-1, 1);
-    image(snailImages[i], -110, -100, 200, 200);
+    image(img, -110, -100, 200, 200);
     scale(-1, 1);
     fill(0);
+    textSize(18);
     textAlign(LEFT, CENTER);
     text(snailNames[i], 90, 50);
   } else {
-    image(snailImages[i], -90, -100, 200, 200);
+    image(img, -90, -100, 200, 200);
     fill(0);
+    textSize(18);
     textAlign(LEFT, CENTER);
     text(snailNames[i], 90, 50);
   }
@@ -217,8 +303,8 @@ function drawSnail(x, y, i) {
 
 function drawSnails() {
 
- for (let i = 0; i < numSnails; i++) {
- let y = 150 + i * 150; //space between snails
+  for (let i = 0; i < numSnails; i++) {
+  let y = getSnailY(i);
 
   let trailEnd = (!snailActive[i] && i === snailRandomIndex)
   ? snailRandomEndX
@@ -251,7 +337,7 @@ if (!snailRandomActive) {
       snailActive[j] = false;
       snailRandomIndex = j;
       snailRandom.x = snailEnds[j];
-      snailRandom.y = 150 + j * 150;
+      snailRandom.y = getSnailY(j);
       snailRandom.history = [];
       snailRandomActive = true;
       snailRandomEndX = snailEnds[snailRandomIndex];
@@ -259,8 +345,10 @@ if (!snailRandomActive) {
       snailRandom.angle = random(-PI / 6, PI / 6);
       snailRandom.baseAngle = snailRandom.angle;
 
-      snailMessage = `${snailNames[j]} ${random(randomSnailMessages)}`;
-      snailMessageTimer = 240;
+      let newMessage = `${snailNames[j]} ${random(randomSnailMessages)}`;
+      snailMessages.push(newMessage);
+      messageYPositions.push(height + 50);
+      if (snailMessages.length > maxMessages) snailMessages.shift();
       break;
     }
   }
@@ -281,8 +369,10 @@ if (!snailRandomActive) {
       if (!snailWrongWay.includes(true) && snailEnds[i] > startLine + 300 && random() < 0.001 && i !== snailRandomIndex) {
         snailWrongWay[i] = true;
 
-      snailMessage = `${snailNames[i]} ${random(wrongWayMessages)}`;
-      snailMessageTimer = 240;
+        let newMessage = `${snailNames[i]} ${random(wrongWayMessages)}`;
+        snailMessages.push(newMessage);
+        messageYPositions.push(height + 50);
+        if (snailMessages.length > maxMessages) snailMessages.shift();
       }
         }
 
@@ -291,8 +381,11 @@ if (!snailRandomActive) {
       snailFlip[i] = true; 
       oneSnailFlip = true;
 
-      snailMessage = `${snailNames[i]} ${random(flipMessages)}`;
-      snailMessageTimer = 240;
+      let newMessage = `${snailNames[i]} ${random(flipMessages)}`;
+      snailMessages.push(newMessage);
+      messageYPositions.push(height + 50);
+      if (snailMessages.length > maxMessages) snailMessages.shift();
+      
     }  
 
 //SNAIL EATEN
@@ -320,15 +413,14 @@ if (!oneSnailEaten && snailEnds[i] > startLine + 200 && random() < 0.001 && i !=
     oneSnailEaten = true;
     snailActive[i] = false;
 
-    let messageTemplate = random(eatenMessages);
-    snailMessage = messageTemplate
-      .replace("{VICTIM}", victim)
-      .replace("{EATER}", eater);
-    snailMessageTimer = 240;
+let messageTemplate = random(eatenMessages);
+let newMessage = messageTemplate
+  .replace("{VICTIM}", victim)
+  .replace("{EATER}", eater);
+snailMessages.push(newMessage);
+if (snailMessages.length > maxMessages) snailMessages.shift();
   }
 }
-
-
 
         }  
     }
@@ -382,7 +474,20 @@ function updateSnailRandomTrailOnly() {
 
 function draw(){
 
-    clear(); 
+    background(255);
+//column gutter dividers
+    let gutter = 40;
+    let colWidth = (width - gutter * 4) / 3;
+    
+    let leftX = gutter;
+    let centerX = leftX + colWidth + gutter;
+    let rightX = centerX + colWidth + gutter;
+
+    stroke(200); 
+    strokeWeight();
+    line(leftX + colWidth + gutter / 2, 0, leftX + colWidth + gutter / 2, height);
+    line(centerX + colWidth + gutter / 2, 0, centerX + colWidth + gutter / 2, height);
+
 
     updateSnailRandomTrailOnly();
 
@@ -393,41 +498,113 @@ function draw(){
     //fill(0, 255, 0);
     //rect(finishLine, 0, 20, height);
 
-
-//snails go and stop text
-   // if(detections.length > 0){
+    if(detections.length > 0){
       //If at least 1 face is detected
-     //   textSize(16);
-       // textAlign (LEFT, TOP)
-        //noStroke();
-        //fill('red');
-        //text("nothing to see here...", 20, 20);
-      //}else{
+     
+      }else{
         //If no faces is detected
-       // textSize(16);
-        //textAlign (LEFT, TOP)
-        //noStroke();
-        //fill('blue');
-        //text("SNAILS GO!", 20, 20);
-        //moveSnails();
-    //  }
+
+
+    
+        moveSnails();
+    }
+ //   if (isSmiling) {
+ //     textSize(16);
+ //     fill('black');
+ //     noStroke();
+ //     textAlign(CENTER, TOP);
+ //     text("you smiled", width / 2, 30);
+ //   }
 
     
       drawSnails();
+      push();
+      if (flyActive) {
+
+        if (detections.length === 0) {
+        fly.angle = map(noise(flyNoiseOffset), 0, 1, 0, TWO_PI);
+        flyNoiseOffset += 0.02;
+        fly.speed = 2;
+
+        fly.x += cos(fly.angle) * fly.speed;
+        fly.y += sin(fly.angle) * fly.speed;
+      
+        fly.x = constrain(fly.x, 0, width);
+        fly.y = constrain(fly.y, 0, height);
+      
+        fly.history.push(createVector(fly.x, fly.y));
+        //if (fly.history.length > 200) fly.history.shift();
+        }
+
+        // Draw trail
+        stroke(0);
+        strokeWeight(2);
+        noFill();
+        beginShape();
+        for (let v of fly.history) {
+          vertex(v.x, v.y);
+        }
+        endShape();
+      
+
+        push();
+        imageMode(CENTER);
+        translate(fly.x, fly.y);
+        rotate(fly.angle);
+        image(flyImg, 0, 0, 30, 30);
+      pop();
+      }
+    
+      pop();
+      
       updateSnailRandom();
       
       drawLeaderboard(); 
       checkWinner(); 
+//timer
+if (frameCount - lastTimerUpdate >= 30) { 
+    if (detections.length > 0) {
+      staringTime++;
+    } else {
+      raceTime++;
+    }
+    lastTimerUpdate = frameCount;
+  }
+//left column 
+noStroke();
+fill(0);
+textSize(16);
+textAlign(LEFT, TOP);
 
-    if (snailMessageTimer > 0) {
-      textSize(16);
-      noStroke();
-      fill(0);
-      textAlign(CENTER, BOTTOM);
-      text(snailMessage, width/2, height -30);
-      snailMessageTimer--;
-    } 
+text(`STARING TIME: ${formatTime(staringTime)}`, leftX, 20);
+text(`RACE TIME: ${formatTime(raceTime)}`, leftX, 40);
+
+if (snailMessages.length > messageYPositions.length) {
+    let newCount = snailMessages.length - messageYPositions.length;
+    for (let i = 0; i < newCount; i++) {
+      messageYPositions.push(height + 50); 
+    }
+  }
+
+let targetBaseY = height - 60;
+
+for (let i = 0; i < snailMessages.length; i++) {
+  let targetY = targetBaseY - (snailMessages.length - 1 - i) * 30;
+  messageYPositions[i] = targetY;
+
+  fill(0);
+  textAlign(LEFT, BOTTOM);
+  textSize(16);
+  text(snailMessages[i].toUpperCase(), leftX, messageYPositions[i]);
+  
 }
+if (millis() > 40000) {
+    snailMessages = [];
+    messageYPositions = [];
+  }
+}
+ 
+
 
 function checkWinner() {
     for (let i = 0; i < numSnails; i++) {         
@@ -456,25 +633,36 @@ function checkWinner() {
 
 
   function drawLeaderboard() {
+    let gutter = 40;
+    let colWidth = (width - gutter * 4) / 3;
+    let rightX = gutter * 3 + colWidth * 2;
+    let topY = 30; 
+
     let rankings = snailEnds
       .map((pos, i) => ({ name: snailNames[i], pos, index: i }))
       .sort((a, b) => b.pos - a.pos);
   
-    fill(0);
-    textSize(16);
-    textAlign(LEFT, TOP);
-    text("Leaderboard:", width - 200, 20);
+   fill(0);
+  noStroke();
+  textSize(45);
+  textStyle(BOLD);
+  text("Leaderboard", rightX, topY);
   
-    for (let i = 0; i < rankings.length; i++) {
-      let snailIndex = rankings[i].index;
-      let targetY = 40 + i * 20;
-  
-     
-      leaderboardY[snailIndex] = lerp(leaderboardY[snailIndex], targetY, 0.3);
-  
-      text(`${i + 1}. ${rankings[i].name}`, width - 200, leaderboardY[snailIndex]);
-    }
-  }
+  textSize(16);
+  textStyle(NORMAL);
+  for (let i = 0; i < rankings.length; i++) {
+    let snailIndex = rankings[i].index;
+    let targetY = topY + 50 + i * 20;
+    leaderboardY[snailIndex] = lerp(leaderboardY[snailIndex], targetY, 0.3);
 
+    let name = rankings[i].name.toUpperCase();
+    text(`${nf(i + 1, 2)}. ${name}`, rightX, leaderboardY[snailIndex]);
+  }
+}
+  function formatTime(seconds) {
+    let mins = floor(seconds / 60);
+    let secs = seconds % 60;
+    return nf(mins, 2) + ":" + nf(secs, 2);
+  }
 
   // random snail: https://www.youtube.com/watch?v=vqE8DMfOajk
